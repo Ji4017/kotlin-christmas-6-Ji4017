fe8ab076dc21324dfcf214507a3ecdd739345322
bak <bak4017@gmail.com>
2023-11-12 19:39:35 +0900

    refactor(christmas): 에러 메시지 상수로 관리

--- src/main/kotlin/christmas/validator/DateValidator.kt
+++ src/main/kotlin/christmas/validator/DateValidator.kt
@@ -11,11 +11,11 @@ class DateValidator(private val visitDay: String) {
     }
 
     private fun checkDigit() {
-        requireNotNull(visitDay.toIntOrNull()) { "[ERROR] 유효하지 않은 날짜입니다. 다시 입력해 주세요." }
+        requireNotNull(visitDay.toIntOrNull()) { INVALID_DATE_FORM_ERROR }
     }
 
     private fun checkDayRange() {
-        require(visitDay.toInt() in 1..31) { "[ERROR] 유효하지 않은 날짜입니다. 다시 입력해 주세요." }
+        require(visitDay.toInt() in 1..31) { INVALID_DATE_FORM_ERROR }
     }
 
     private fun checkCurrentDate() {
@@ -24,8 +24,12 @@ class DateValidator(private val visitDay: String) {
         val currentDay = currentDate.dayOfMonth
 
         if (currentMonth == EVENT_MONTH) {
-            println("if문 실행")
-            require(currentDay < visitDay.toInt()) { "[ERROR] 현재 날짜보다 이전의 날짜는 선택할 수 없습니다." }
+            require(currentDay < visitDay.toInt()) { INVALID_DAY_ERROR }
         }
+    }
+
+    companion object {
+        private const val INVALID_DATE_FORM_ERROR = "[ERROR] 유효하지 않은 날짜입니다. 다시 입력해 주세요."
+        private const val INVALID_DAY_ERROR = "[ERROR] 현재 날짜보다 이전의 날짜는 선택할 수 없습니다."
     }
 }
