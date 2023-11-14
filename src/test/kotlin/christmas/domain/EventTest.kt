12170fdc22f452eca78993a0ccb30fb1f42bc456
bak <bak4017@gmail.com>
2023-11-14 23:20:36 +0900

    refactor(christmas): EventTest 클래스의 음수부호 수정

--- src/test/kotlin/christmas/domain/EventTest.kt
+++ src/test/kotlin/christmas/domain/EventTest.kt
@@ -8,56 +8,56 @@ class EventTest {
     @Test
     fun `크리스마스 할인 적용 테스트`() {
         val visitDay = 6
-        val orderInformation = Order(mapOf("양송이수프" to 2, "타파스" to 1), visitDay)
+        val orderInformation = Order(visitDay, mapOf("양송이수프" to 2, "타파스" to 1))
         val benefit = Event(orderInformation)
         val discounts = benefit.getDiscounts()
-        assertEquals(-(Event.BASE_DISCOUNT + (visitDay-1) * Event.PER_DAY_DISCOUNT), discounts[Event.TITLE_CHRISTMAS_DISCOUNT])
+        assertEquals(Event.BASE_DISCOUNT + ((visitDay-1) * Event.PER_DAY_DISCOUNT), discounts[Event.TITLE_CHRISTMAS_DISCOUNT])
     }
 
     @Test
     fun `평일 할인 적용 테스트`() {
         val dessertCount = 3
-        val orderInformation = Order(mapOf("초코케이크" to dessertCount, "양송이수프" to 2), 6)
+        val orderInformation = Order(6, mapOf("초코케이크" to dessertCount, "양송이수프" to 2))
         val benefit = Event(orderInformation)
         val discounts = benefit.getDiscounts()
-        assertEquals(-(Event.WEEKDAY_DISCOUNT * dessertCount), discounts[Event.TITLE_WEEKDAY_DISCOUNT])
+        assertEquals(Event.WEEKDAY_DISCOUNT * dessertCount, discounts[Event.TITLE_WEEKDAY_DISCOUNT])
     }
 
     @Test
     fun `주말 할인 적용 테스트`() {
         val mainCount = 2
         val saturDay = 9
-        val orderInformation = Order(mapOf("티본스테이크" to mainCount, "양송이수프" to 2), saturDay)
+        val orderInformation = Order(saturDay, mapOf("티본스테이크" to mainCount, "양송이수프" to 2))
         val benefit = Event(orderInformation)
         val discounts = benefit.getDiscounts()
-        assertEquals(-(Event.WEEKEND_DISCOUNT * mainCount), discounts[Event.TITLE_WEEKEND_DISCOUNT])
+        assertEquals(Event.WEEKEND_DISCOUNT * mainCount, discounts[Event.TITLE_WEEKEND_DISCOUNT])
     }
 
     @Test
     fun `특별 할인 적용 테스트`() {
         val starDay = 3
-        val orderInformation = Order(mapOf("양송이수프" to 2, "타파스" to 1), starDay)
+        val orderInformation = Order(starDay, mapOf("양송이수프" to 2, "타파스" to 1))
         val benefit = Event(orderInformation)
         val discounts = benefit.getDiscounts()
-        assertEquals(-(Event.SPECIAL_DISCOUNT), discounts[Event.TITLE_SPECIAL_DISCOUNT])
+        assertEquals(Event.SPECIAL_DISCOUNT, discounts[Event.TITLE_SPECIAL_DISCOUNT])
     }
 
     @Test
     fun `증정 이벤트 적용 테스트`() {
-        val orderInformation = Order(mapOf("해산물파스타" to 2, "레드와인" to 1, "초코케이크" to 1), 6)
+        val orderInformation = Order(6, mapOf("해산물파스타" to 2, "레드와인" to 1, "초코케이크" to 1))
         val benefit = Event(orderInformation)
         val discounts = benefit.getDiscounts()
-        assertEquals(-(Event.GIFT_PRICE), discounts[Event.TITLE_GIFT_EVENT])
+        assertEquals(Event.GIFT_PRICE, discounts[Event.TITLE_GIFT_EVENT])
     }
 
     @Test
     fun `증정품 경곗값 테스트`() {
         // 주문의 총 가격이 선물이 증정되는 120,000원 이상으로 되도록 (양송이수프=6000, 레드와인=60000)
-        val orderWithGift = Order(mapOf("양송이수프" to 10, "레드와인" to 1), 6)
+        val orderWithGift = Order(6, mapOf("양송이수프" to 10, "레드와인" to 1))
         val given = Event(orderWithGift)
 
         // 주문의 총 가격이 선물 증정 가격 이하
-        val orderWithoutGift = Order(mapOf("양송이수프" to 2, "타파스" to 1), 6)
+        val orderWithoutGift = Order(6, mapOf("양송이수프" to 2, "타파스" to 1))
         val nothing = Event(orderWithoutGift)
 
         assertEquals(Event.GIFT_NAME, given.getGift())
