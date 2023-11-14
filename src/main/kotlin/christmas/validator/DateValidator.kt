6b46e417efa7320bae436e6c4ad3a4d8eaa92e52
bak <bak4017@gmail.com>
2023-11-14 22:48:25 +0900

    refactor(christmas): DateValidator 클래스에 문자열 상수 추가

--- src/main/kotlin/christmas/validator/DateValidator.kt
+++ src/main/kotlin/christmas/validator/DateValidator.kt
@@ -1,6 +1,5 @@
 package christmas.validator
 
-import christmas.constant.Constants.EVENT_MONTH
 import java.time.LocalDate
 
 class DateValidator(private val visitDay: String) {
@@ -29,6 +28,7 @@ class DateValidator(private val visitDay: String) {
     }
 
     companion object {
+        private const val EVENT_MONTH = 12
         private const val INVALID_DATE_FORM_ERROR = "[ERROR] 유효하지 않은 날짜입니다. 다시 입력해 주세요."
         private const val INVALID_DAY_ERROR = "[ERROR] 현재 날짜보다 이전의 날짜는 선택할 수 없습니다."
     }
