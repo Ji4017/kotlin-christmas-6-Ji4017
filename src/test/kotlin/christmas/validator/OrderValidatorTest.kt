ed412fd638431424601a77a13a7bcf90e798afbf
bak <bak4017@gmail.com>
2023-11-15 01:13:52 +0900

    refactor(christmas): 중복 코드 리팩토링

--- src/test/kotlin/christmas/validator/OrderValidatorTest.kt
+++ src/test/kotlin/christmas/validator/OrderValidatorTest.kt
@@ -1,7 +1,5 @@
 package christmas.validator
 
-import christmas.validator.OrderValidator.Companion.INVALID_ORDER_ERROR
-import christmas.validator.OrderValidator.Companion.MAXIMUM_COUNT_ERROR
 import org.assertj.core.api.Assertions
 import org.junit.jupiter.api.Test
 import org.junit.jupiter.api.assertDoesNotThrow
@@ -9,48 +7,43 @@ import org.junit.jupiter.params.ParameterizedTest
 import org.junit.jupiter.params.provider.ValueSource
 
 class OrderValidatorTest {
+
+    private fun assertInvalidOrder(value: String, message: String) {
+        Assertions.assertThatIllegalArgumentException().isThrownBy {
+            OrderValidator(value)
+        }.withMessage(message)
+    }
+
     @ParameterizedTest
     @ValueSource(strings = ["가나다", "가나다-1,", "가나다,1", "가나다-1-라마바-1", "가나다-1,가나다"])
     fun `메뉴 형식 검증`(value: String) {
-        Assertions.assertThatIllegalArgumentException().isThrownBy {
-            OrderValidator(value)
-        }.withMessage(INVALID_ORDER_ERROR)
+        assertInvalidOrder(value, OrderValidator.INVALID_ORDER_ERROR)
     }
 
     @Test
     fun `없는 메뉴 입력`() {
-        Assertions.assertThatIllegalArgumentException().isThrownBy {
-            OrderValidator("무지개돈까스-1")
-        }.withMessage(INVALID_ORDER_ERROR)
+        assertInvalidOrder("무지개돈까스-1", OrderValidator.INVALID_ORDER_ERROR)
     }
 
     @Test
     fun `메뉴 중복`() {
-        Assertions.assertThatIllegalArgumentException().isThrownBy {
-            OrderValidator("시저샐러드-1,시저샐러드-1")
-        }.withMessage(INVALID_ORDER_ERROR)
+        assertInvalidOrder("시저샐러드-1,시저샐러드-1", OrderValidator.INVALID_ORDER_ERROR)
     }
 
     @Test
     fun `음료만 주문`() {
-        Assertions.assertThatIllegalArgumentException().isThrownBy {
-            OrderValidator("레드와인-2")
-        }.withMessage(INVALID_ORDER_ERROR)
+        assertInvalidOrder("레드와인-2", OrderValidator.INVALID_ORDER_ERROR)
     }
 
     @ParameterizedTest
     @ValueSource(strings = ["시저샐러드-하나", "시저샐러드-0"])
     fun `메뉴의 개수가 1이상의 숫자가 아닐 때`(value: String) {
-        Assertions.assertThatIllegalArgumentException().isThrownBy {
-            OrderValidator(value)
-        }.withMessage(INVALID_ORDER_ERROR)
+        assertInvalidOrder(value, OrderValidator.INVALID_ORDER_ERROR)
     }
 
     @Test
     fun `최대 주문 개수 초과`() {
-        Assertions.assertThatIllegalArgumentException().isThrownBy {
-            OrderValidator("시저샐러드-10,초코케이크-11")
-        }.withMessage(MAXIMUM_COUNT_ERROR)
+        assertInvalidOrder("시저샐러드-10,초코케이크-11", OrderValidator.MAXIMUM_COUNT_ERROR)
     }
 
     @ParameterizedTest
